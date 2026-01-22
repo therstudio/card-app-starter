db582fdeb15a07b96afaefeeaaaaccefa444e3ab
Randy <24047653@gmail.com>
2026-01-22 15:39:13 +0800

    mew

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,23 @@
 import { NavLink } from "react-router-dom";
+import "./Navbar.css"; 
 
 export default function Navbar() {
-  /* TODO: Complete the navbar 
-    - add links to CardList and AddCard pages 
-    - style as a navbar UI */
-
   return (
-    <header>
-      <strong>Card App</strong>
-      <nav>
-        <NavLink to="/" end>
-          Home
-        </NavLink>
-      </nav>
+    <header className="navbar-header">
+      <div className="navbar-container">
+        <strong className="navbar-brand">Card App</strong>
+        <nav className="navbar-links">
+          <NavLink to="/" end>
+            Home
+          </NavLink>
+          <NavLink to="/cards">
+            Card List
+          </NavLink>
+          <NavLink to="/add">
+            Add Card
+          </NavLink>
+        </nav>
+      </div>
     </header>
   );
 }
